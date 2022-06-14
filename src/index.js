4ccde0412414bf15998b558f67598fd78d1daabd
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-14 03:57:13 +0300

    Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    const inputValue = document.querySelector('input').value;
+    e.preventDefault();
+
+    const childrenList = document.createElement('li');
+    const closeButton = document.createElement('button')
+    closeButton.textContent = "X";
+    
+    childrenList.textContent = inputValue;
+    const list = document.querySelector('#tasks').appendChild(childrenList)
+    list.appendChild(closeButton).addEventListener('click', () => {
+      list.remove()
+    })
+
+
+  })
+  
+
 });
